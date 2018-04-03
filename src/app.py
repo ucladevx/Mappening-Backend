e73437fb8d05eaa59bb15e02a795dbf2dab1954d
Jason Xu <xu.jason.jx@gmail.com>
2018-04-02 23:29:06 -0700

    do nothing for now, temp

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from mappening import app
 from mappening.utils import scheduler
+from mappening.api.utils import event_utils
 
 from flask import Flask
 
@@ -14,10 +15,14 @@ if __name__ == "__main__":
     event_update_thread = Thread(target = scheduler.event_thread_func)
     event_update_thread.start()
 
-    code_update_date = "2/15/18"
+    code_update_date = "4/2/18"
     print("Updated on: {0}".format(code_update_date))
 
-    print("Mappening is happening!")
+    print("Mappening is happening!\n")
+
+    print('UPDATE EVENTS?? Not yet\n')
+    # need to pass in args from command line
+    # event_utils.update_ucla_events_database()
 
     # Flask defaults to port 5000
     # If debug is true, runs 2 instances at once (so two copies of all threads)
